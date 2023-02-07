1ea1c9091a438d592bb6b4bf4b87fa5f57d69adc
Gabriel Navas <gabnanavas.dev@gmail.com>
2023-02-07 17:26:39 -0300

    add mongoose config, docker-compose with mongo image and env with variable to connection on mongodb

--- src/server.js
+++ src/server.js
@@ -3,20 +3,35 @@ import http from 'http'
 import { Server } from 'socket.io'
 import cors from 'cors'
 
-const PORT_SERVER = process.env.PORT || 4000
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
+dotenv.config()
+
+// mongo db
+const connectionStr = process.env.MONGO_URL
+mongoose.connect(connectionStr)
+  .then(() => console.log('[ * ] - connected to mongod'))
+  .catch(err => console.log(err))
+  
+
+// http server
+const PORT_SERVER = process.env.PORT || 4000
 const app = express()
 const server = http.createServer(app)
 
+// socket server
 const io = new Server(server, {
   cors: '*',
   methods: '*'
 })
 
+// middles app
 app.use(cors())
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json())
 
+// start server
 server.listen(PORT_SERVER, () => {
-  console.log(`server running on port: ${PORT_SERVER}`)
+  console.log(`[ * ] - server running on port: ${PORT_SERVER}`)
 })
